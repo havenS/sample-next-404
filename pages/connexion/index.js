1f3e43c984266208a2b89b238eacfd413f69c1b7
Julien Fournier <fournier.j@sfr.fr>
2017-01-01 09:52:06 +0100

    Sample repo

---
+++ pages/connexion/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/prefetch'
+
+export default () => (
+  <div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/prefetch'
+
+export default () => (
+  <div>
+    <Link href="/connexion">
+      <a>connect</a>
+    </Link>
+  </div>
+)
